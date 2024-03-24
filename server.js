f047f75c10b7a229936bd413bdab9c5407acf3d6
efackenya <support@efac.org>
2024-03-24 22:15:42 +0300

    minor change

--- server.js
+++ server.js
@@ -44,7 +44,7 @@ app.use(express.json());
 app.use("/", index);
 app.use("/image", image);
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 6000;
 app.listen(PORT, async () => {
   console.log(`Server is listening at http://localhost:${PORT}`);
   await connectdb();
